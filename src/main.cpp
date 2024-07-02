8bda391a21878d07d4d63b0925ded2d2339d1bd3
yangqing <yangqing6@cmcm.com>
2024-07-02 21:18:38 +0800

    [0701]autocommit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
-#include "FreeFTP.h"
-#include <QtWidgets/QApplication>
+// #include "FreeFTP.h"
+// #include <QtWidgets/QApplication>
+//
+// int main(int argc, char *argv[])
+//{
+//     QApplication a(argc, argv);
+//     FreeFTP w;
+//     w.show();
+//     return a.exec();
+// }
 
-int main(int argc, char *argv[])
+
+#include <QApplication>
+#include <QDebug>
+#include <QFileDialog>
+#include <QStandardItemModel>
+#include <QTreeView>
+#include <QPushButton>
+int
+main (int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    FreeFTP w;
-    w.show();
-    return a.exec();
+	QApplication app (argc, argv);
+
+	QPushButton btn ("Open File");
+	btn.show();
+
+	QFileDialog dialog (nullptr, "Open File");
+
+	QObject::connect (&btn, &QPushButton::clicked, &dialog, &QFileDialog::raise);
+	dialog.setFileMode (QFileDialog::ExistingFiles);
+
+	dialog.show();
+
+	return app.exec();
 }
